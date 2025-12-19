541daf0e8059bdf4999af8584d42fce69b3d0501
s-taktak <3560594019@g.ecc.u-tokyo.ac.jp>
2025-12-19 21:56:24 +0900

    category4

--- api/routers/category.py
+++ api/routers/category.py
@@ -6,5 +6,5 @@ from api.cruds.category import get_category
 router = APIRouter()
 
 @router.get("/categories",operation_id="getCategories", tags=["category"])
-def get_categories(parent_id: int = None, db: AsyncSession = Depends(get_db)):
-    return get_category(parent_id=parent_id)
+async def get_categories(parent_id: int = None, db: AsyncSession = Depends(get_db)):
+    return await get_category(db,parent_id=parent_id)
