31416c706600219e3548149b6f92afee0ed1765a
s-taktak <3560594019@g.ecc.u-tokyo.ac.jp>
2025-12-19 14:27:41 +0900

    modifyAllow

--- api/main.py
+++ api/main.py
@@ -35,7 +35,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://my-app.vercel.app",
+        "https://hackathon-frontend-pink.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
